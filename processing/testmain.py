2f2ff591106a0ab718cc9fb6a8eaebc7e2dfd309
nbesteman <nbesteman@gmail.com>
2016-06-16 12:27:55 -0400

    added testmain.py to test classUnit.py import

---
+++ processing/testmain.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        main
+# Purpose:     Main module used to translate data from a geodatabase into shapefile and tab formats.  It also changes the projection.
+# Author:      Neil Besteman
+# Created:     20160607
+# Modified:    20160615
+#-------------------------------------------------------------------------------
+#src = r'C:\data\00 Allegan Co\LGIM\AC_Cadastral.gdb'
+#dst= r'C:\apps\python\ESRI\Translate\source\AC_Cadastral.gdb'
+
+#import os
+import classUnit
+
+#classUnit.unit01.displayUnit()
+#classUnit.unit59.displayUnit()
+classUnit.unit01.displayUnit()
+for value in classUnit.unitName:
+        print value
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+#UnitList.units(fmcd,unit,code,twprng,propername,shortname,unittype)
+#time.sleep(1)
