c4e449282107f21d2108bf9a319b2117e9d2a497
nbesteman <nbesteman@gmail.com>
2016-06-17 15:19:54 -0400

    added y/n input dialog asking if new .gdb is desired

--- processing/copyGDB2source.py
+++ processing/copyGDB2source.py
@@ -1,17 +1,30 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        copyGDB2sourc.py
 # Purpose:     copy AC_Cadastral.gdb to source folder
 # Author:      Neil Besteman
 # Created:     20160607
-# Modified:    20160609
+# Modified:    20160617 - adding request for input y/n regarding new copy of gdb
 #-------------------------------------------------------------------------------
 import shutil
+
 src = r'C:\data\00 Allegan Co\LGIM\AC_Cadastral.gdb'
-dst= r'C:\apps\python\ESRI\Translate\source\test\AC_Cadastral.gdb'
+dst= r'C:\apps\python\ESRI\Translate\source\AC_Cadastral.gdb'
+inputYN = 'n'
+y = 'y'
+inputYN = str(raw_input('copytreeCopy new .gdb to source folder? [y/n]:'))
 
 def copytree(src,dst):
-    shutil.copytree(src,dst)
-    pass
+    print 'this is from the copytree'
+    if inputYN == 'y':
+        shutil.copytree(src,dst)
+        print 'copied AC_Cadastral.gdb from C:\\data\\00 Allegan Co\\LGIM\\ to C:\\apps\\python\\ESRI\\Translate\\source\\'
+    else:
+        print 'last .gdb in source folder is being used'
+
 if __name__ == '__main__':
-    copytree(src,dst)
-    print 'copied AC_Cadastral.gdb from C:\\data\\00 Allegan Co\\LGIM\\ to C:\\apps\\python\\ESRI\\Translate\\source\\'
+    print 'this is from the local main'
+    if inputYN == 'y':
+        shutil.copytree(src,dst)
+        print 'copied AC_Cadastral.gdb from C:\\data\\00 Allegan Co\\LGIM\\ to C:\\apps\\python\\ESRI\\Translate\\source\\'
+    else:
+        print 'last .gdb in source folder is being used'
